ae93c119f7574e26bd68877edbb325621a04630d
Saul Pierotti <saul@ebi.ac.uk>
2024-06-21 10:37:29 +0100

    hmm splits

--- scripts/set_hmm_cv_splits.R
+++ scripts/set_hmm_cv_splits.R
@@ -3,10 +3,12 @@
 library("data.table")
 
 set.seed(1)
-n_folds <- 2
 
-df <- fread("../samplesheets/trajectories_for_hmm.csv")[
-  , .(id, cv_fold = sample(1:n_folds, .N, replace = TRUE))
-]
+df <- fread("../samplesheets/trajectories_for_hmm.csv")
+n_samples <- nrow(df)
+n_samples_a <- n_samples %/% 2
+n_samples_b <- n_samples - n_samples_a
+cv_fold <- c(rep("A", n_samples_a), rep("B", n_samples_b)) |> sample()
+df <- df[, .(id, cv_fold = ..cv_fold)]
 
 fwrite(df, "../samplesheets/hmm_cv_splits.csv")
